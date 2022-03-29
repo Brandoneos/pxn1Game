0ff7dfc8b249b1a31980427683401760299bfb0f
brandon <beruddle@gmail.com>
2022-03-29 14:58:01 -0500

    added first child(dexpfp)

--- pxn1/GameScene.swift
+++ pxn1/GameScene.swift
@@ -13,7 +13,10 @@ class GameScene: SKScene {
     override func didMove(to view: SKView) {
         view.backgroundColor = UIColor(red: 93/255, green: 60/255, blue: 222/255, alpha: 1.0)
         var firstImage = SKSpriteNode(imageNamed: "dexPFP")
-        
+        firstImage.setScale(1)
+        firstImage.position = CGPoint(x: self.size.width/2, y: self.size.height/2)
+        firstImage.zPosition = 2
+        self.addChild(firstImage)
     }
         
 }
